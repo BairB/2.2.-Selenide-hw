820e1a8262d5de640caae053e56c0dd94ed8c9df
Bair B <bairu@yandex.ru>
2021-06-14 11:54:46 +0300

    test modified

--- src/test/java/ru/domain/netology/CardDeliveryTest.java
+++ src/test/java/ru/domain/netology/CardDeliveryTest.java
@@ -1,56 +1,47 @@
 package ru.domain.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import java.time.Duration;
+import java.time.LocalDate;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class CardDeliveryTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-      //  driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
-    void shouldTestCardDelivery(){
+    void shouldTestCardDelivery() {
         open("http://localhost:9999");
         $(".input__control[placeholder=\"Город\"]").setValue("Улан-Удэ");
-        $(".input__control[placeholder=\"Дата встречи\"]").setValue("13.06.2021");
+        String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy"));
+        $(".input__control[placeholder=\"Дата встречи\"]").setValue(date);
         $(".input__control[name='name']").setValue("Иванов Иван");
         $(".input__control[name='phone']").setValue("+79261234567");
         $(".checkbox__box").click();
         $(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(20));
     }
+
+    @Test
+    void shouldTestCardDeliveryWithDate() {
+        open("http://localhost:9999");
+        $(".input__control[placeholder=\"Город\"]").setValue("Улан-Удэ");
+        String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy"));
+        $(".input__control[placeholder=\"Дата встречи\"]").setValue(date);
+        $(".input__control[name='name']").setValue("Иванов Иван");
+        $(".input__control[name='phone']").setValue("+79261234567");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $(withText(date)).shouldBe(Condition.visible, Duration.ofSeconds(20));
+    }
 }
+
