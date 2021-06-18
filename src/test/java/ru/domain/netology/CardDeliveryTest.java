d80be37bcb2d8c1e0151ead4ca1500f43bf3db05
Bair B <bairu@yandex.ru>
2021-06-18 20:16:50 +0300

    test modified

--- src/test/java/ru/domain/netology/CardDeliveryTest.java
+++ src/test/java/ru/domain/netology/CardDeliveryTest.java
@@ -5,12 +5,13 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
 import java.time.Duration;
 import java.time.LocalDate;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -22,6 +23,7 @@ public class CardDeliveryTest {
     void shouldTestCardDelivery() {
         open("http://localhost:9999");
         $(".input__control[placeholder=\"Город\"]").setValue("Улан-Удэ");
+        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
         String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy"));
         $(".input__control[placeholder=\"Дата встречи\"]").setValue(date);
         $(".input__control[name='name']").setValue("Иванов Иван");
@@ -35,13 +37,15 @@ public class CardDeliveryTest {
     void shouldTestCardDeliveryWithDate() {
         open("http://localhost:9999");
         $(".input__control[placeholder=\"Город\"]").setValue("Улан-Удэ");
+        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
         String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy"));
         $(".input__control[placeholder=\"Дата встречи\"]").setValue(date);
         $(".input__control[name='name']").setValue("Иванов Иван");
         $(".input__control[name='phone']").setValue("+79261234567");
         $(".checkbox__box").click();
         $(".button").click();
-        $(withText(date)).shouldBe(Condition.visible, Duration.ofSeconds(20));
+        $("[data-test-id='notification'] .notification__content").shouldBe(visible, Duration.ofSeconds(20))
+                .shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 }
 
